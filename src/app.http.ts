e867d71b0d786154ca3f8111a48179b5f8008c28
Roy Reategui del Aguila <roy.reateguia@gmail.com>
2023-11-09 16:10:24 -0500

    fisrt commit

---
+++ src/app.http.ts
@@ -0,0 +1,39 @@
+import http from 'http'
+import fs from 'node:fs';
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === '/') {
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlFile)
+  }
+
+
+  else if (req.url?.endsWith('.css')) {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+  } else if (req.url?.endsWith('.js')) {
+    res.writeHead(200, { 'Content-Type': 'application/javascript' });
+  }
+
+  fs.readFile(`./public/${req.url}`, 'utf-8', (error, content) => {
+    if (error) {
+      // res.writeHead(404, { 'Content-Type': 'text/html' });
+      // res.setHeader('Content-Type', 'text/html');
+      res.statusCode = 404;
+      return res.end('not fount');
+    }
+    res.end(content);
+  });
+
+
+
+})
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+})
+
+
+
+
